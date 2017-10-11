1832b2d0921e883a9fed4c78500df447e3176273
yonatan Mengesha <mail2yonz@gmail.com>
2017-10-11 11:55:43 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+/*
+ Name:Yonatan Mengesha
+
+ Age:33
+
+
+
+ */
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Print out a message
+
+        System.out.println("Hello Yonatan");
+    }
+}
